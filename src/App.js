3487615a8789725ddbaebca546eb8adf2d76e97a
EnesArslan8 <enes8arslan@gmail.com>
2023-08-25 11:54:25 +0300

    DragAndDrop,DragAndDropMenu compenents added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import React from 'react';
+import DragAndDrop from "./compenents/DragAndDrop.js";
+
 
 function App() {
   return (
     <div className="App">
       <div className="dragAndDrop">
-
+        <DragAndDrop/>
       </div>
       <div className="validation">
         
