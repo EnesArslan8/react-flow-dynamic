992edbca187dd9cc42c01b8d022f31121bdabb27
EnesArslan8 <enes8arslan@gmail.com>
2023-08-27 16:37:15 +0300

    DagreTree removed and added Validation compenent

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import "./App.css";
 import React from "react";
 import DragAndDrop from "./compenents/DragAndDrop.js";
 import NodeContextMenu from "./compenents/NodeContextMenu";
-import DagreTree from "./compenents/DagreTree";
-
 function App() {
   return (
     <div className="App">
@@ -13,9 +11,8 @@ function App() {
       <div className="dragAndDrop">
         <DragAndDrop />
       </div>
-      <div className="dagreTree">
-        <DagreTree />
-        {/* <p>Dagre Tree</p> */}
+      <div className="validation">
+        
       </div>
     </div>
   );
--- src/compenents/DagreTree.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function DagreTree() {
-  return (
-    <div>DagreTree</div>
-  )
-}
-
-export default DagreTree
